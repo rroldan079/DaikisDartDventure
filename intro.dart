bda43c38b7e88ce1bdaf3efbe7311436ee5dba05
Roger Roldan <133607448+rroldan079@users.noreply.github.com>
2024-12-14 12:03:39 -0800

    Create intro.dart

---
+++ intro.dart
@@ -0,0 +1,26 @@
+void main() {
+  int anitegervariable = 9;
+  
+  String thisisastringvariable = "2";
+  int thisisalsoanintegervariable = int.parse(thisisastringvariable); 
+  // HTFEDRHTEYTHHTHYTRHYTRHYTRHHRTHTRHTRFHTRHTDFTDH
+  //DATA CASTING
+
+  print((anitegervariable.toString()) + thisisastringvariable);
+  print(thisisalsoanintegervariable + anitegervariable);
+
+// Create three variables, one a string, one a integer, and another as a integer.
+  
+// The string variables will store number characters within them
+  
+// Find a way to print two of the number added together combined as a string with the third
+// Find a way to print all three numbers added together
+ // Find a way to print all three numbers combined as a string
+  String hi = "15";
+  int hello = 23;
+  int sup = 32;
+  int ohio = int.parse(hi);
+  print(hello + sup + ohio);
+  print((hello + sup).toString() + hi);
+  
+}
